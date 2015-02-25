0b0e61c5bf666fa896642b57dfc7388823af676a
Arven <arven@muckdragon.info>
2015-02-25 14:22:38 -0500

    Forgot to add package configs.

---
+++ src/main/java/com/github/arven/example/types/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema(namespace = "http://github.com/Arven/spring-cxf-example", elementFormDefault = XmlNsForm.QUALIFIED,
+        xmlns = @XmlNs(namespaceURI = "http://github.com/Arven/spring-cxf-example", prefix = "api"))
+package com.github.arven.example.types;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
