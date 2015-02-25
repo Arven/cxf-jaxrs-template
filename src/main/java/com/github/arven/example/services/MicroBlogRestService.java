d226d408480fbeb9ccf45cc3b5f0053a0cf7eae8
Arven <arven@muckdragon.info>
2015-02-25 11:30:03 -0500

    Minor spacing stuff.

--- src/main/java/com/github/arven/example/services/MicroBlogRestService.java
+++ src/main/java/com/github/arven/example/services/MicroBlogRestService.java
@@ -3,15 +3,19 @@ package com.github.arven.example.services;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 
+import javax.ws.rs.Path;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+
 import javax.ws.rs.PathParam;
+import javax.ws.rs.MatrixParam;
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.MatrixParam;
-import javax.ws.rs.PUT;
+
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
