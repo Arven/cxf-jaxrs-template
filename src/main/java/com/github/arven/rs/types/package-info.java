3c5a52d76c64f3753829edc9158ca496f6a4ce19
Arven <brian.johnathan.b@gmail.com>
2015-02-26 17:23:07 -0500

    Remove namespace stuff, simpler overview.

--- src/main/java/com/github/arven/rs/types/package-info.java
+++
@@ -1,7 +0,0 @@
-@XmlSchema(namespace = "http://github.com/Arven/spring-cxf-example/xt", elementFormDefault = XmlNsForm.QUALIFIED,
-        xmlns = @XmlNs(namespaceURI = "http://github.com/Arven/spring-cxf-example/xt", prefix = "xt"))
-package com.github.arven.rs.types;
-
-import javax.xml.bind.annotation.XmlNs;
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
