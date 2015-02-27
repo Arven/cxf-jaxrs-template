476d3017c0af1188b6997a368c6c5174be1f2e09
Arven <arven@muckdragon.info>
2015-02-27 11:44:53 -0500

    Fix Schema Support, XML

---
+++ src/main/java/com/github/arven/rs/types/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema(namespace = "http://github.com/Arven/cxf-jaxrs-template", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.UNQUALIFIED, xmlns = @XmlNs(namespaceURI="http://github.com/Arven/cxf-jaxrs-template", prefix = "api"))
+package com.github.arven.rs.types;
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
