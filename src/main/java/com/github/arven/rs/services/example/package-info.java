39a9017cf7b8ac19c1a316a9fa83322d5c447806
Arven <brian.johnathan.b@gmail.com>
2015-03-13 17:21:18 -0400

    Add more deployment stuff, JacksonJaxb module

---
+++ src/main/java/com/github/arven/rs/services/example/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema( namespace="http://github.com/Arven/jaxrs-template", elementFormDefault=XmlNsForm.QUALIFIED )
+package com.github.arven.rs.services.example;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
